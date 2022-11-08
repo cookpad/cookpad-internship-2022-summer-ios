492cbb149ab8616d7109eea2e61e07b8b57e4436
Yuki Miida <ryokucha.break.dev@gmail.com>
2022-11-08 19:06:26 +0900

    Chapter 7

--- MiniCookpad/View/RecipeList/RecipeListView.swift
+++ MiniCookpad/View/RecipeList/RecipeListView.swift
@@ -2,17 +2,39 @@ import SwiftUI
 
 struct RecipeListView: View {
     @StateObject private var viewModel = RecipeListViewModel()
+    @State private var isInitialDataLoaded: Bool = false
 
     var body: some View {
-        List(viewModel.items) { item in
+        List(Array(viewModel.data.items.enumerated()), id: \.element.id) { index, item in
             // レシピ詳細画面に遷移
             NavigationLink(destination: RecipeDetailView(recipeId: item.recipe.id)) {
                 RecipeListRow(item: item)
             }
+
+            // リストの最後にAutoLoadingIndicatorを追加する
+            if index == viewModel.data.items.count - 1 {
+                AutoLoadingIndicator(
+                    isFetching: viewModel.data.isFetching,
+                    didReachLast: viewModel.data.didReachLast,
+                    loadMoreAction: {
+                        await viewModel.request(.loadMore)
+                    }
+                )
+            }
         }
         .listStyle(PlainListStyle())
+        // Pull-to-refreshでユーザーがデータを再ロードできるようにする
+        .refreshable {
+            await viewModel.request(.refresh)
+        }
         .task {
-            await viewModel.request()
+            // 初回表示の時のみリクエストを送る
+            // レシピ詳細画面からレシピ一覧画面に戻ってきた時にもtask Modifierのクロージャは呼ばれるので
+            // これがないとレシピ詳細画面から戻ってくる度に追加ロードしたレシピが消えてしまう
+            if !isInitialDataLoaded {
+                await viewModel.request(.refresh)
+                isInitialDataLoaded = true
+            }
         }
         .navigationTitle("レシピ一覧")
     }
--- MiniCookpad/View/RecipeList/RecipeListViewModel.swift
+++ MiniCookpad/View/RecipeList/RecipeListViewModel.swift
@@ -2,11 +2,53 @@ import SwiftUI
 
 @MainActor
 final class RecipeListViewModel: ObservableObject {
-    @Published var items: [RecipeListItem] = []
+    struct DataState {
+        var items: [RecipeListItem] = []
+        var isFetching: Bool = false // データ取得中かどうか
+        var pageInfo: GetRecipeListResponse.PageInfo? = nil
 
-    func request() async {
+        var didReachLast: Bool {
+            pageInfo?.hasNextPage == false
+        }
+    }
+
+    enum RequestKind {
+        case refresh // 初回ロード（または再ロード）
+        case loadMore // 追加ロード
+    }
+
+    @Published var data: DataState = .init()
+
+    func request(_ kind: RequestKind) async {
+        // データ取得中に呼ばれたら何もしない
+        if data.isFetching { return }
+
+        // GetRecipeListRequestの引数に渡すpageInfo
+        let pageInfo: GetRecipeListResponse.PageInfo?
+        switch kind {
+        case .refresh:
+            // 初回ロード（または再ロード）の時は、GetRecipeListRequestの引数に渡すpageInfoはnilにする
+            pageInfo = nil
+        case .loadMore:
+            // didReachLastがtrueの時は、これ以上取得できるデータはないのでAPIリクエストは送らない
+            if data.didReachLast {
+                assertionFailure("no more data to fetch")
+                return
+            }
+            // 追加ロードの時は、前回のAPIリクエストで取得したpageInfoをGetRecipeListRequestの引数に渡す
+            // GetRecipeListRequestは、クエリパラメータcursorにpageInfo.nextPageCursorの値を指定してAPIリクエストを送る
+            pageInfo = data.pageInfo
+        }
+
+        // isFetchingをtrueにしてデータ取得中の状態にする
+        data = DataState(
+            items: data.items,
+            isFetching: true,
+            pageInfo: pageInfo
+        )
+
         do {
-            let recipeListResponse = try await apiClient.send(request: GetRecipeListRequest(pageInfo: nil))
+            let recipeListResponse = try await apiClient.send(request: GetRecipeListRequest(pageInfo: pageInfo))
             let recipeHastagsResponse = try await apiClient.send(request: GetRecipeHashtagsRequest(recipeIds: recipeListResponse.recipes.map(\.id)))
 
             var newItems: [RecipeListItem] = []
@@ -15,7 +57,21 @@ final class RecipeListViewModel: ObservableObject {
                 newItems.append(.init(recipe: recipe, hashtags: recipeHastags.hashtags))
             }
             
-            items = newItems
+            let items: [RecipeListItem]
+            switch kind {
+            case .refresh:
+                items = newItems
+            case .loadMore:
+                // 追加ロードの時は前回までのデータに今回取得したデータを足す
+                items = data.items + newItems
+            }
+
+            // データの更新
+            data = DataState(
+                items: items,
+                isFetching: false,
+                pageInfo: recipeListResponse.pageInfo
+            )
         } catch {
             print(error)
         }
